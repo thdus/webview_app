5a27ec6dddc044686f21b4012478c6b89ab4008f
soyeon <sykim0659@naver.com>
2024-09-05 16:13:19 +0900

    commit

--- app/src/main/java/com/example/webview_app/MainActivity.kt
+++ app/src/main/java/com/example/webview_app/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.webview_app
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +13,22 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val address = findViewById<EditText>(R.id.address)
+
+        address.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                Log.d("testt", "beforeTextChanged : " + s)
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                Log.d("testt", "onTextChanged : " + s)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                Log.d("testt", "afterTextChanged : " + s)
+            }
+        })
     }
 }
