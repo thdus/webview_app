39b25093a2390dd6ca9675185f69a13766d96467
soyeon <sykim0659@naver.com>
2024-09-05 18:26:47 +0900

    commit

--- app/src/main/java/com/example/webview_app/MainActivity.kt
+++ app/src/main/java/com/example/webview_app/MainActivity.kt
@@ -4,19 +4,28 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
+import android.webkit.WebView
 import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.widget.doAfterTextChanged
+import androidx.core.widget.doBeforeTextChanged
+import androidx.core.widget.doOnTextChanged
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val webView = findViewById<WebView>(R.id.webview)
+        val urlPrefix = "http://"
+        var finalUrl = ""
 
         val address = findViewById<EditText>(R.id.address)
 
+        // addTextChangedListener 사용법
         address.addTextChangedListener(object : TextWatcher{
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
                 Log.d("testt", "beforeTextChanged : " + s)
@@ -27,8 +36,21 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun afterTextChanged(s: Editable?) {
+                finalUrl = urlPrefix +s.toString()
                 Log.d("testt", "afterTextChanged : " + s)
             }
         })
+
+        // addTextChangedListener 사용법 -> 람다 형식
+        address.doBeforeTextChanged { text, start, count, after ->  }
+        address.doOnTextChanged { text, start, before, count ->  }
+        address.doAfterTextChanged {  }
+
+        val open = findViewById<TextView>(R.id.open)
+        open.setOnClickListener {
+            val url = address.text.toString()
+            Log.d("testt", url)
+            webView.loadUrl(finalUrl)
+        }
     }
 }
